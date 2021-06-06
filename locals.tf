c45e9a0a106a0f8073706daf421db1986769726f
liranp <liran.polak@gmail.com>
2021-06-06 12:10:23 +0300

    chore: format variables for better readability

--- locals.tf
+++ locals.tf
@@ -1,10 +1,39 @@
 locals {
-  subnets             = var.subnets != null ? var.subnets : module.vpc.private_subnets
-  vpc_id              = var.vpc_id != null ? var.vpc_id : module.vpc.vpc_id
-  ami_id              = var.ami_id != null ? var.ami_id : module.eks.workers_default_ami_id
-  cluster_name        = var.cluster_name != null ? var.cluster_name : "ocean-${random_string.suffix.result}"
-  ocean_cluster_id    = element(concat(spotinst_ocean_aws.this.*.id, [""]), 0)
-  ocean_controller_id = var.cluster_identifier != null ? var.cluster_identifier : module.eks.cluster_id
+  subnets = (
+    var.subnets != null
+    ? var.subnets
+    : module.vpc.private_subnets
+  )
+
+  vpc_id = (
+    var.vpc_id != null
+    ? var.vpc_id
+    : module.vpc.vpc_id
+  )
+
+  ami_id = (
+    var.ami_id != null
+    ? var.ami_id
+    : module.eks.workers_default_ami_id
+  )
+
+  cluster_name = (
+    var.cluster_name != null
+    ? var.cluster_name
+    : "ocean-${random_string.suffix.result}"
+  )
+
+  ocean_cluster_id = (
+    var.create_ocean
+    ? element(concat(spotinst_ocean_aws.this.*.id, [""]), 0)
+    : null
+  )
+
+  ocean_controller_id = (
+    var.cluster_identifier != null
+    ? var.cluster_identifier
+    : module.eks.cluster_id
+  )
 }
 
 resource "random_string" "suffix" {
